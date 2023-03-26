736682fed42baec06f22d15250713eadb4494bb9
A.D <anton.demchenkov@gmail.com>
2023-03-26 21:11:00 +0300

    #2 Refactored project structure. Docker-compose configuration.

--- project/app/config/AppConfig.py
+++ project/app/config/AppConfig.py
@@ -1,7 +1,7 @@
 import logging
 from functools import lru_cache
 
-from pydantic import BaseSettings
+from pydantic import AnyUrl, BaseSettings
 
 log = logging.getLogger("uvicorn")
 
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
 	app_name: str = "Warehouse Management"
 	environment: str = "DEV"  # DEV, TEST, PROD
 	testing: bool = bool(0)
+	database_url: AnyUrl = None
 
 
 #  @lru_cache - декоратор для кеширования результата выполнения функции в память
---
+++ project/postgres/create.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE wm_postgres_dev;
+CREATE DATABASE wm_postgres_test;
