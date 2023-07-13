4f04e7b46ce593c82851a9d13cd13e8d8a91da1d
A.D <anton.demchenkov@gmail.com>
2023-07-13 12:49:38 +0300

    Defined warehouse-service methods' names

---
+++ project/app/src/warehouse_services/service.py
@@ -0,0 +1,43 @@
+# Назначить (изменить) МОЛ на складе
+def define_responsible_to_storage():
+	pass
+
+
+# Принять материальные средства на склад
+def get_assets_to_warehouse():
+	pass
+
+
+# Выдать со склада
+def deliver_assets_from_warehouse():
+	pass
+
+
+# Вернуть на склад
+def return_assets_to_warehouse():
+	pass
+
+
+# Переместить материальные средства в использовании
+def transfer_assets_in_use():
+	pass
+
+
+# Переместить материальные ценности между складами
+def transfer_assets_between_storages():
+	pass
+
+
+# Списать со склада
+def write_off_assets_from_storage():
+	pass
+
+
+# Списать с рабочего места
+def write_off_assets_from_workplace():
+	pass
+
+
+# Отменить операцию (сделать обратную проводку)
+def reverse_operation():
+	pass
